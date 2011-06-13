b093dedc8c8cec73417701ecf78586c65e1380ac
Aengus McMillin <AengusMcMillin@gmail.com>
2011-06-13 12:17:23 -0700

    Created the first failing test and fixed it

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -41,4 +41,11 @@ describe PagesController do
                                     :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+  end
 end
